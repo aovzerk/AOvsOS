9275ffd698c1c818a02956a00be66b6309126d83
aovzerk <pikseloooooo@gmail.com>
2023-03-08 03:28:12 +0400

    feat: добавлена функция вывода строки

--- SRC/KERNEL/KERNEL.C
+++ SRC/KERNEL/KERNEL.C
@@ -11,26 +11,14 @@ asm volatile(
 #include "FUNCTIONS/ETC.H"
 
 void main(){
-    static u_char8 num = 0x90;
-    static u_char8 mode = 0x03;
-    static u_char8 helloStr[] = "123456789";
-    static u_char8 helloStr2[] = "123456789";
+    u_char8 num = 0x90;
+    u_char8 mode = 0x03;
+    static u_char8 helloStr[] = "Hello World! =)";
+    static CURSOR_POS pos = {10, 0};
     setInterruptHandler(num, mainInterruptHandler);
     setVideoMode(mode);
-    CURSOR_POS pos = {0, 0};
-    pos.x = 5;
-    SetCursorPos(pos);
-    pos = GetCursorPos();
-    pos.x += (u_char8) 50;
-    SetCursorPos(pos);
-    u_char8 len = stringLen(helloStr);
-    for(u_char8 i = 0; i < len; i++){
-        printChar(helloStr[i]);
+    for(u_char8 i = 0; i < 100; i++){
+        printString(helloStr, Green);
     }
-    printChar((u_char8)0x00);
-    printChar(len + 0x30);
-    u_char8 eqStrings = stringCmp(helloStr, helloStr2);
-    printChar((u_char8)0x00);
-    printChar(eqStrings + 0x30);
     asm volatile("jmp $\n");
 }
